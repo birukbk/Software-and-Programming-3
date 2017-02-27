ef026d867aa2a6b2b731f760a74c5033f13155f3
Biruk G <birukbk@gmail.com>
2017-02-27 22:40:06 +0000

    Implements BnzInstraction.

--- coursework/cw-one/SML_Scala/src/sml/BnzInstraction.scala
+++ coursework/cw-one/SML_Scala/src/sml/BnzInstraction.scala
@@ -5,9 +5,7 @@ case class BnzInstruction(label: String, opcode: String, val register: Int, val 
   override def execute(m: Machine) {
     val value1 = m.regs(register)
     val value2 = labelToExcute.drop(1).toInt
-
-    //m.regs(result) = value1
-    if (value1 != 0) m.start = value2
+    if (value1 != 0) m.pc = value2
   }
   override def toString(): String = {
     super.toString + " register " + register + " register2 "  + "\n"
@@ -15,6 +13,6 @@ case class BnzInstruction(label: String, opcode: String, val register: Int, val 
 }
 
 object BnzInstruction {
-  def apply(label: String, register: Int, register2: String) =
-  new BnzInstruction(label, "bnz", register, register2)
+  def apply(label: String, register: Int, labelToExcute: String) =
+  new BnzInstruction(label, "bnz", register, labelToExcute)
 }
